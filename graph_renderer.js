cea7700a2af12924b3470c0b5aed5fa6ab30be9d
ronan-morris <78508701+ronan-morris@users.noreply.github.com>
2023-01-28 11:49:40 -0500

    canvas testing

--- graph_renderer.js
+++ graph_renderer.js
@@ -4,7 +4,7 @@ class GraphRenderer{
      * Initializes a Graph Renderer object
     */
     constructor(document, canvasID){
-        this.ctx = document.getElementById(canvasID).getContext
+        this.ctx = document.getElementById(canvasID).getContext("2d")
     }
 
     /**
@@ -14,11 +14,18 @@ class GraphRenderer{
         * @param    {float}   rotation              How far the time axis is rotated 'out of the screen' in radians. Default is 0.5
     */
     renderAxes(timeScaleFactor, maxAmplitude, rotation){
+        this.ctx.beginPath();
+        this.ctx.moveTo(threeDimsCoordsToTwo(0,0,0))
+        this.ctx.lineTo(threeDimsCoordsToTwo(200,0,0))
+        this.ctx.moveTo(threeDimsCoordsToTwo(0,-80,0))
+        this.ctx.lineTo(threeDimsCoordsToTwo(0,80,0))
+        this.ctx.moveTo(threeDimsCoordsToTwo(0,0,-80))
+        this.ctx.lineTo(threeDimsCoordsToTwo(0,0,80))
 
     }
 
-    threeDimsCoordsToTwo(x,y,z, zScale = 0.25, perspectiveRotation = 0.5){
-        x_2d = x + Math.cos(perspectiveRotation) * z * zScale;
+    threeDimsCoordsToTwo(x,y,z, xOffset = 50, zScale = 0.25, perspectiveRotation = 0.5){
+        x_2d = x + Math.cos(perspectiveRotation) * z * zScale + xOffset;
         y_2d = y + Math.sin(perspectiveRotation) * z * zScale;
         return x_2d,y_2d;
     }
